ced569a4d0f6daf2b94882f405e1e925679e8ef7
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-02-10 14:40:20 -0700

    Added User Menu

--- CPRG211_Group1_Assignment1/Program.cs
+++ CPRG211_Group1_Assignment1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// INSERT APPLIANCES.TXT TO LIST HERE
+
+while (true)
+{
+    Console.WriteLine("Welcome to Modern Appliances!");
+    Console.WriteLine("How May We Assist You?");
+    Console.WriteLine("1 - Check out appliance \n2 - Find appliances by brand \n3 - Display appliances by type \n4 - Produce random appliance List \n5 - Save & exit");
+    Console.WriteLine("Enter option: ");
+    string option = Console.ReadLine();
+
+    if (option == "1")
+    {
+        //INSERT CHECK OUT APPLIANCE HERE
+    }
+    else if (option == "2")
+    {
+        //INSERT FIND APPLIANCES BY BRAND HERE
+    }
+    else if (option == "3")
+    {
+       //INSERT DISPLAY APPLIANCES BY TYPE HERE
+    }
+    else if (option == "4")
+    {
+        //INSERT PRODUCE RANDOM APPLIANCE LIST HERE 
+
+    }
+    else if(option == "5")
+    {
+        // INSERT SAVE LIST TO FILE HERE
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid option");
+    }
+}
