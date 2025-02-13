1ee9091fd7b3141c1f3b51af83307664eae9725b
sinferna <jaspritgrewal@hotmail.com>
2025-02-13 13:32:34 -0700

    Made Capacity a double value and rounded string to 1 decimal place, added Kitchen and Work Site room types to constructor in Microwave.cs

--- CPRG211_Group1_Assignment1/Microwave.cs
+++ CPRG211_Group1_Assignment1/Microwave.cs
@@ -3,22 +3,30 @@
     internal class Microwave : Appliance
     {
         // create Microwave attributes
-        private int _capacity;
+        private double _capacity;
 
         private string _roomType;
-        public int Capacity { get { return _capacity; } set { _capacity = value; } }
+        public double Capacity { get { return _capacity; } set { _capacity = value; } }
         public string RoomType { get { return _roomType; } set { _roomType = value; } }
-        public string RoomTypeKitchen = "K";
-        public string RoomTypeWorkSite = "W";
-        public string DisplayRoomType { get; set; }
+        //public string RoomTypeKitchen = "K";
+        //public string RoomTypeWorkSite = "W";
+        //public string DisplayRoomType { get; set; }
 
         // constructor
-        public Microwave(string itemNumber, int quantity, string brand, int wattage, string colour, double price, int capacity, string roomType)
+        public Microwave(string itemNumber, int quantity, string brand, int wattage, string colour, double price, double capacity, string roomType)
             : base(itemNumber, brand, quantity, wattage, colour, price)
         {
             this.Capacity = capacity;
             this.RoomType = roomType;
-        }
+            if (roomType == "K")
+            {
+                this.RoomType = "Kitchen";
+            }
+            if (roomType == "W");
+            {
+                this.RoomType = "Work Site";
+            }
+    }
 
         // FormatToFile method
         public string FormatToFile()
@@ -35,7 +43,7 @@
                    $"Wattage: {Wattage}\n" +
                    $"Colour: {Colour}\n" +
                    $"Price: {Price}\n" + 
-                   $"Capacity: {Capacity}\n" +
+                   $"Capacity: {Capacity.ToString("f1")}\n" +
                    $"Room Type: {RoomType}";
         }
     }
--- CPRG211_Group1_Assignment1/Refrigerator.cs
+++ CPRG211_Group1_Assignment1/Refrigerator.cs
@@ -4,51 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CPRG211_Group1_Assignment1
+namespace CPRG211_Group1_Assignment1;
+
+public class Refrigerator : Appliance
 {
-    public class Refrigerator : Appliance
+    private int quantity;
+
+    //public string ItemNumber { get; set; }
+    //public string Brand { get; set; }
+    public int Quantity { get { return quantity; } set { quantity = value; } }
+    //public int Wattage { get; set; }
+    //public string Colour { get; set; }
+    //public double Price { get; set; }
+    public string DoorsNumber { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+
+    public Refrigerator(string itemNumber, string brand, int quantity, int wattage, string colour, double price, string doors, int height, int width) :
+        base(itemNumber, brand, wattage, colour, price)
     {
-        private int quantity;
-
-        //public string ItemNumber { get; set; }
-        //public string Brand { get; set; }
-        public int Quantity { get { return quantity; } set { quantity = value; } }
-        //public int Wattage { get; set; }
-        //public string Colour { get; set; }
-        //public double Price { get; set; }
-        public string DoorsNumber { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
-
-        public Refrigerator(string itemNumber, string brand, int quantity, int wattage, string colour, double price, string doors, int height, int width) :
-            base(itemNumber, brand, wattage, colour, price)
+        //this.ItemNumber = itemNumber;
+        //this.Brand = brand;
+        this.Quantity = quantity;
+        //this.Wattage = wattage;
+        //this.Colour = colour;
+        //this.Price = price;
+        this.Height = height;
+        this.Width = width;
+        if(doors == "2")
         {
-            //this.ItemNumber = itemNumber;
-            //this.Brand = brand;
-            this.Quantity = quantity;
-            //this.Wattage = wattage;
-            //this.Colour = colour;
-            //this.Price = price;
-            this.Height = height;
-            this.Width = width;
-            if(doors == "2")
-            {
-                this.DoorsNumber = "Double Doors";
-            }
-            else if(doors == "3")
-            {
-                this.DoorsNumber = "Three Doors";
-            }
-            else
-            {
-                this.DoorsNumber = "Four Doors";
-            }
-
+            this.DoorsNumber = "Double Doors";
+        }
+        else if(doors == "3")
+        {
+            this.DoorsNumber = "Three Doors";
+        }
+        else
+        {
+            this.DoorsNumber = "Four Doors";
         }
 
-        public override string ToString()
-        {
-            return $" ItemNumber: {ItemNumber} \nBrand: {Brand} \nQuantity: {Quantity} \nWattage: {Wattage} \nColour: {Colour} \nPrice: {Price} \nNumber of Doors: {DoorsNumber} \nHeight: {Height} \nWidth: {Width}";
-        }
+    }
+
+    public override string ToString()
+    {
+        return $" ItemNumber: {ItemNumber} \nBrand: {Brand} \nQuantity: {Quantity} \nWattage: {Wattage} \nColour: {Colour} \nPrice: {Price} \nNumber of Doors: {DoorsNumber} \nHeight: {Height} \nWidth: {Width}";
     }
 }
