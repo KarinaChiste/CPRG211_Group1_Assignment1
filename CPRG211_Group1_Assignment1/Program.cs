496697731c77f0761de3aa53dbc2ffe8f794a58b
Ashtonrp <ashton.pritchard@edu.sait.ca>
2025-02-13 16:57:45 -0500

    Fixes vacuum batteryVoltage adds save to file adds if statement to option 3

--- CPRG211_Group1_Assignment1/Program.cs
+++ CPRG211_Group1_Assignment1/Program.cs
@@ -36,7 +36,7 @@ List<Appliance> ReadFile()
         }
 
     }
-
+    read.Close();
     return appliances;
 }
 
@@ -70,7 +70,7 @@ while (true)
         if (appliance.Quantity > 0)
         {
             appliance.Quantity--;
-            Console.WriteLine($"Applicane {itemNumber} has been checked out.");
+            Console.WriteLine($"Appliance {itemNumber} has been checked out.");
         }
         else
         {
@@ -127,7 +127,16 @@ while (true)
         else if (applianceOption == "2")
         {
             Console.WriteLine("Enter battery voltage value - 18 V (low) or 24 V (high):");
-            int voltageValue = int.Parse(Console.ReadLine());
+            string voltageValue = Console.ReadLine();
+            if (voltageValue == "18")
+            {
+                voltageValue = "Low";
+            }
+
+            else if (voltageValue == "24")
+            {
+                voltageValue = "High";
+            }
 
             Console.WriteLine("Matching vacuums:");
             // create new list with all items from appliance list that match user input (cast Vacuum class)
@@ -199,6 +208,14 @@ while (true)
     else if (option == "5")
     {
         // INSERT SAVE LIST TO FILE HERE
+        using (StreamWriter writer = new StreamWriter(PATH))
+        {
+            foreach (Appliance appliance in appliances)
+            {
+                writer.WriteLine(appliance.ToString());
+            }
+        }
+        Console.WriteLine("Appliance list has been saved. Exiting...");
         break;
     }
     else
